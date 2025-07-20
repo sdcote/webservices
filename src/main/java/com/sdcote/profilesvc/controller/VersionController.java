fee4b2b0d489b0ecd45a18870e7942c5f292bebe
sdcote <sdcote@gmail.com>
2025-07-20 14:23:22 -0400

    Version controller now works

--- src/main/java/com/sdcote/profilesvc/controller/VersionController.java
+++ src/main/java/com/sdcote/profilesvc/controller/VersionController.java
@@ -1,10 +1,13 @@
 package com.sdcote.profilesvc.controller;
 
-import org.springframework.boot.actuate.info.BuildProperties;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 
+/**
+ * http://localhost:8080/version
+ */
 @RestController
 public class VersionController {
 
