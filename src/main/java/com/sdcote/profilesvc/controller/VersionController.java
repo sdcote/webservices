7b21642a256d83b5f4ee5d1797907b26fb391228
sdcote <sdcote@gmail.com>
2025-07-24 12:18:17 -0400

    Check bothenvironment varibles and system properties

--- src/main/java/com/sdcote/profilesvc/controller/VersionController.java
+++ src/main/java/com/sdcote/profilesvc/controller/VersionController.java
@@ -12,11 +12,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class VersionController {
 
     private final BuildProperties buildProperties;
-    private static final String ENVIRONMENT_PROPERTY_STRING = "ENVIRON";
+    private static final String ENVIRONMENT_KEY = "ENVIRON";
+    private static final String UNKNOWN_STRING = "UNKNOWN";
 
     @Autowired
     public VersionController(BuildProperties buildProperties) {
         this.buildProperties = buildProperties;
+    }
+
+    /**
+     * Retrieve the value of the named environment variable or system property.
+     * 
+     * <p>Check the environment variables first, and if not there, check the system
+     * properties. If in neither, just return "UNKNOWN".
+     * 
+     * @return the value of the given variable name.
+     */
+    private static String getVariable(String key) {
+        String retval = null;
+        if (key != null && !key.isEmpty()) {
+            retval = System.getenv(key);
+
+            // If not found in environment variables, try system properties
+            if (retval == null) {
+                retval = System.getProperty(key);
+            }
+        }
+        if (retval == null) {
+            retval = UNKNOWN_STRING;
+        }
+        return retval;
     }
 
     @GetMapping("/version")
@@ -24,7 +49,8 @@ public class VersionController {
         StringBuilder builder = new StringBuilder("Component Version: ");
         builder.append(buildProperties.getVersion());
         builder.append("  Environment: ");
-        builder.append(System.getProperty(ENVIRONMENT_PROPERTY_STRING));
-        return  builder.toString() ;
+        builder.append(getVariable(ENVIRONMENT_KEY));
+        return builder.toString();
     }
+    
 }
